4b7063a0b4373abd71a2d7d3bba08d324a0f5f89
becc-mu <becc.mulugeta@gmail.com>
2019-03-01 16:11:08 +0000

    todoitem with key

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 
 class TodoItem extends Component {
   render() {
     return (
       <div>
-        <h1>TodoItem</h1>
+        <p>{ this.props.todo.title }</p>
       </div>
     );
   }
